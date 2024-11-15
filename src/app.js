836ab50152920217430d0345c6db9b1c1e2bc7e7
xorsx <oliviarswann@gmail.com>
2024-11-15 16:18:08 -0500

    changed theme and css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function displayPoem(response) {
   console.log("poem generated");
 
-  new Typewriter("#haiku", {
+  new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
@@ -14,11 +14,15 @@ function generatePoem(event) {
 
   let userInstructionsInput = document.querySelector("#user-instructions");
   let apiKey = "e7fo6tea7dab2f64b9d0694388b0ea0f";
-  let context = `You are a haiku poem expert and love to write unique haiku poems. You need to generate a haiku poem using ${userInstructionsInput} Make sure to follow instructions.`;
-  let prompt = `User instructions: please generate a haiku poem about ${userInstructionsInput}`;
+  let context = `You are a Spanish poem expert. You need to generate a short Spanish poem using ${userInstructionsInput} no more than 5 lines. Make sure to follow instructions.`;
+  let prompt = `User instructions: please generate a short poem in Spanish about ${userInstructionsInput}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
+
+  let poemELement = document.querySelector("#poem");
+  poemELement.classList.remove("hidden");
+  poemELement.innerHTML = `Generating a poem about ${userInstructionsInput}`;
 
   axios.get(apiUrl).then(displayPoem);
 }
