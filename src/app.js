ae84415063315fa1f2ec23d15d26b2577b9d407d
xorsx <oliviarswann@gmail.com>
2024-11-15 16:53:29 -0500

    changed theme back to haiku

--- src/app.js
+++ src/app.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let userInstructionsInput =
     document.querySelector("#user-instructions").value;
   let apiKey = "e7fo6tea7dab2f64b9d0694388b0ea0f";
-  let context = `You are a Spanish poem expert. You need to generate a short Spanish poem using ${userInstructionsInput} no more than 5 lines. Make sure to follow instructions.`;
-  let prompt = `User instructions: please generate a short poem in Spanish about ${userInstructionsInput}`;
+  let context = `You are a haiku poem expert. A haiku poem consists of seventeen syllables, in three lines of five, seven, and five, traditionally evoking images of the natural world. You need to generate a short haiku poem using ${userInstructionsInput} no more than 5 lines. Make sure to follow instructions.`;
+  let prompt = `User instructions: please generate a haiku poem about ${userInstructionsInput}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemELement = document.querySelector("#poem");
   poemELement.classList.remove("hidden");
-  poemELement.innerHTML = `💭Pensamiento...`;
+  poemELement.innerHTML = `💭Thinking...`;
 
   axios.get(apiUrl).then(displayPoem);
 }
